83934fa58bc4c2f4f160fbc2d807d0ac0f05dec0
Manuel Loaiza Vasquez <19366788+ManuelLoaizaV@users.noreply.github.com>
2024-09-05 20:35:53 +0000

    Add sign up page

---
+++ src/front/js/pages/sign-up.jsx
@@ -0,0 +1,97 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export default function SignUp() {
+    const navigate = useNavigate();
+
+    function handleOnClick() {
+        navigate("/login");
+    }
+
+    return (
+        <div className="vh-100 d-flex justify-content-center align-items-center">
+            <div className="container">
+                <form>
+                    <img
+                        className="img-fluid mb-4"
+                        src="https://static-00.iconduck.com/assets.00/bitwarden-v2-icon-512x512-cstnj11p.png"
+                        alt="Bitwarden logo"
+                        style={{ height: '57px' }}
+                    />
+                    <h1 className="mb-3">Sign up</h1>
+                    <div className="row g-3">
+                        <div className="col-sm-6">
+                            <label
+                                for="first-name"
+                                className="form-label"
+                            >
+                                First name
+                            </label>
+                            <input
+                                id="first-name"
+                                type="text"
+                                className="form-control"
+                                required
+                            />
+                        </div>
+                        <div className="col-sm-6">
+                            <label
+                                for="last-name"
+                                className="form-label"
+                            >
+                                Last name
+                            </label>
+                            <input
+                                id="last-name"
+                                type="text"
+                                className="form-control"
+                                required
+                            />
+                        </div>
+                        <div className="col-12">
+                            <label
+                                for="email"
+                                className="form-label"
+                            >
+                                Email&nbsp;
+                                <span className="text-secondary">(Required)</span>
+                            </label>
+                            <input
+                                id="email"
+                                type="email"
+                                className="form-control"
+                                placeholder="example@4geeks.xyz"
+                                required
+                            />
+                        </div>
+                        <div className="col-12">
+                            <label
+                                for="password"
+                                className="form-label"
+                            >
+                                Password&nbsp;
+                                <span className="text-secondary">(Required)</span>
+                            </label>
+                            <input
+                                id="password"
+                                type="password"
+                                className="form-control"
+                                required
+                            ></input>
+                        </div>
+                        <button
+                            className="btn btn-primary w-100 mb-3"
+                            type="submit"
+                        >
+                            Create account
+                        </button>
+                    </div>
+                    <p>
+                        Already have an account?&nbsp;
+                        <a className="link-opacity-100" onClick={handleOnClick}>Log in</a>
+                    </p>
+                </form>
+            </div>
+        </div>
+    );
+}
